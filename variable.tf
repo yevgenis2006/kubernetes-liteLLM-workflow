36dabf8aaabe4a032834463cd81db26044ba9b73
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-02 23:51:18 +0200

    update

--- variable.tf
+++ variable.tf
@@ -4,3 +4,23 @@ variable "kubeconfig_path" {
   description = "Path to kubeconfig for the target cluster"
   default     = "~/.kube/config"
 }
+
+variable "openai_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "anthropic_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
